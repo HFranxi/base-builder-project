1e48dd90a3934741f3f18774efb29a80baf6a3f7
HFranxi <82986762+HFranxi@users.noreply.github.com>
2026-03-12 11:55:16 +0700

    Update SimpleNFT.sol

--- contracts/SimpleNFT.sol
+++ contracts/SimpleNFT.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract SimpleNFT {
+
+    string public name = "BaseBuilderNFT";
+    string public symbol = "BBNFT";
+
+    uint public totalSupply;
+
+    mapping(uint => address) public ownerOf;
+
+    function mint() public {
+
+        totalSupply++;
+
+        ownerOf[totalSupply] = msg.sender;
+
+    }
+
+}
