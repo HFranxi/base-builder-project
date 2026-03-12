bd05a7f64a5bb3e8ea59178951b0f68ea75af2b7
HFranxi <82986762+HFranxi@users.noreply.github.com>
2026-03-12 11:54:50 +0700

    Update StorageFactory.sol

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract SimpleStorage {
+
+    string public message;
+
+    constructor(string memory _message){
+        message = _message;
+    }
+
+}
+
+contract StorageFactory {
+
+    address[] public storages;
+
+    function createStorage(string memory _message) public {
+
+        SimpleStorage s = new SimpleStorage(_message);
+
+        storages.push(address(s));
+
+    }
+
+}
