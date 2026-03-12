278e85f7e226f5238a29715a240f3e1619a42171
HFranxi <82986762+HFranxi@users.noreply.github.com>
2026-03-12 11:56:05 +0700

    Update App.jsx

--- contracts/frontend/App.jsx
+++ contracts/frontend/App.jsx
@@ -1,1 +1,11 @@
+import { ethers } from "ethers"
 
+async function connectWallet(){
+
+const provider = new ethers.BrowserProvider(window.ethereum)
+
+await provider.send("eth_requestAccounts", [])
+
+}
+
+export default connectWallet
